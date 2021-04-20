a8e7286f71989e2290bec3b6a4c57af1da418995
Marcelaxz <marcela590@outlook.com>
2021-04-20 11:53:08 -0300

    React Refresher

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+/*const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];*/
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-1d5aa-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for(const key in data){
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []); // in dependencies array, u should add all extern values your effect function relies on
+          // in this case, there are no external values
+
+  if (isLoading) {
+    return (
+      <section>
+        <p> Loading...</p>
+      </section>
+    );
+  }
+
+  /*return (
+    <section>
+      <h1> All Meetups </h1>
+      <MeetupList meetups={DUMMY_DATA} />
+    </section>
+  );*/
+
+  return (
+    <section>
+      <h1> All Meetups </h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
